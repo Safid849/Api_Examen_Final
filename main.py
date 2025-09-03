6ac2937d9d65b2455384d8a186aefae4b3caa11b
Zinedis <nantenainabakari@gmail.com>
2025-09-03 09:51:29 +0200

    Add route phone and her methode http

--- main.py
+++ main.py
@@ -37,3 +37,23 @@ def serialize_posts():
     for p in list_phone:
         post_serialized.append(p.model_dump())
     return post_serialized
+
+@app.get("/phones")
+async def get_phones():
+    return list_phone
+
+@app.get("/phones/{identifier}")
+async def get_phone(identifier: str):
+    for phone in list_phone:
+        if phone.identifier == identifier:
+            return JSONResponse(content=phone.model_dump(), status_code=200,media_type="application/json")
+        else:
+            return Response(content=f"phone with identifier {identifier} is not exist",status_code=404, media_type="text/plain")
+
+
+@app.put("/phones/{id}/characteristics")
+async def put_phone(id: str, characteristic: dict):
+    for phone in list_phone:
+        if phone.identifier == id:
+            phone.characteristic = characteristic
+            return JSONResponse(content=phone.model_dump(), status_code=200,media_type="application/json")
